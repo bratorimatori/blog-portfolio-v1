4683e6b224151652861c04aa041fd6fff0eeadb5
Bojan Tomic <bojan.tomic@vroom.com>
2023-06-30 15:44:38 +0200

    new post 1

--- components/PostBody.tsx
+++ components/PostBody.tsx
@@ -4,10 +4,7 @@ type Props = {
 
 const PostBody = ({ content }: Props) => {
   return (
-    <div
-      className='max-w-2xl mx-auto'
-      dangerouslySetInnerHTML={{ __html: content }}
-    />
+    <div className='mx-auto' dangerouslySetInnerHTML={{ __html: content }} />
   );
 };
 
